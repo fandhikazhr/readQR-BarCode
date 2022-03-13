dd7bcaded9eeaa0ef63e86b2445cf2bb88d80b3a
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-13 13:49:25 +0700

    Update codeAuth.py

--- withAuthentication/codeAuth.py
+++ withAuthentication/codeAuth.py
@@ -1,3 +1,10 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+with open('data.text') as f:
+    myDataList = f.read().splitlines()
