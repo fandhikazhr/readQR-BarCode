3e4e0dd7454297e25ea4fec5180ff3aecf52f760
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-13 13:49:51 +0700

    Update codeAuth.py

--- withAuthentication/codeAuth.py
+++ withAuthentication/codeAuth.py
@@ -8,3 +8,17 @@ cap.set(4,480)
 
 with open('data.text') as f:
     myDataList = f.read().splitlines()
+    
+while True:
+    success, img = cap.read()
+    for barcode in decode(img):
+        # print(barcode.data)
+        myData = barcode.data.decode('utf-8')
+        print(myData)
+
+        if myData in myDataList:
+            myOutput = 'Authorized'
+            colorAuth = (80,255,0)
+        else:
+            myOutput = 'Un-Authorized'
+            colorAuth = (0,0,255)
