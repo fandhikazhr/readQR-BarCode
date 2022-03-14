08357a2c93aead2fd68a345ab2e65fa8b04f0aac
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-15 00:31:41 +0700

    Update lines.py

--- detectQRCodeMidpointBlackLines/lines.py
+++ detectQRCodeMidpointBlackLines/lines.py
@@ -11,3 +11,19 @@ upper_black = np.array([0,0,0], np.uint8)
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
+
+while True:
+    success, img = cap.read()
+    for barcode in decode(img):
+        myData = barcode.data.decode('utf-8')
+        pts = np.array([barcode.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts],True,(80,255,0),5)
+        pts2 = barcode.rect
+        # cv2.putText(img,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,
+        #             0.9,(80,255,0),3)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    white = cv2.inRange(hsv, lower_white, upper_white)
+    cnts1 = cv2.findContours(white, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    countours1 = imutils.grab_contours(cnts1)
