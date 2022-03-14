4b0323ed4a2092c16c5032cc0c3111aa2299bd35
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-14 11:31:20 +0700

    Update detect.py

--- detectQRCodeWithDetectionMidpoint/detect.py
+++ detectQRCodeWithDetectionMidpoint/detect.py
@@ -3,8 +3,8 @@ import numpy as np
 import imutils
 from pyzbar.pyzbar import decode
 
-lower_white = np.array([0,0,0], np.uint8)
-upper_white = np.array([0,0,255], np.uint8)
+lower_white = np.array([0,0,168], np.uint8)
+upper_white = np.array([172,111,255], np.uint8)
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
