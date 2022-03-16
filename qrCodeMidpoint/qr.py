79ef0d9eeb4028706c54fd13b0e8b7087e308aba
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-17 01:02:14 +0700

    Update qr.py

--- qrCodeMidpoint/qr.py
+++ qrCodeMidpoint/qr.py
@@ -19,3 +19,23 @@ if settinghsv:
     cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
     cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 print ("QR Detection Starting...")
+while (1):
+    _, frame = cap.read()
+    success, img = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    if settinghsv:
+        l_h = cv2.getTrackbarPos("LH", "Tracking")
+        l_s = cv2.getTrackbarPos("LS", "Tracking")
+        l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+        u_h = cv2.getTrackbarPos("UH", "Tracking")
+        u_s = cv2.getTrackbarPos("US", "Tracking")
+        u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+        lower_white = np.array([l_h, l_s, l_v])
+        upper_white = np.array([u_h, u_s, u_v])
+
+    else:
+        lower_white = np.array([40,0,220], np.uint8)
+        upper_white = np.array([179, 255, 255], np.uint8)
+        white= cv2.inRange(hsv, lower_white, upper_white)
